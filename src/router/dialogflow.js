68be1f42a63eaaef89cb65fd74e43f1183f4da60
LinhChi <mailinhchi22082002@gmail.com>
2022-10-27 18:03:21 +0700

    fix bug route

--- src/router/dialogflow.js
+++ src/router/dialogflow.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const Dialogflow = require("../controller/Dialogflow");
 
-router.get("/", Dialogflow.home);
 router.post("/text_query", Dialogflow.text);
 router.post("/event_query", Dialogflow.event);
 
